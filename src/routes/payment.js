4b98db676ecdbefb9fe7d7253d85494a6c62be9d
abhay-ti2002 <abhi.tiwari2027@gmail.com>
2026-02-28 23:57:35 +0530

    route

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -52,6 +52,7 @@ paymentRoute.post("/payment/create", userAuth, async (req, res) => {
 
 paymentRoute.post(
   "/payment/webhook",
+  express.raw({ type: "application/json" }),
   async (req, res) => {
     try {
       /* NODE SDK: https://github.com/razorpay/razorpay-node */
